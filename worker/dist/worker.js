dfdc6fb45108d5fdaf5ccbf7b637788e78c6f98c
jacksonyoyo <siriusorionsiriusorion@gmail.com>
2020-05-03 19:57:02 +0800

    Update worker.js

--- worker/dist/worker.js
+++ worker/dist/worker.js
@@ -1,5 +1,5 @@
 self.props = {
-	title: 'GDIndex',
+	title: 'GDIndex',// 自改
 	default_root_id: 'root',
 	client_id: '202264815644.apps.googleusercontent.com',
 	client_secret: 'X4Z3ca8xfWDb1Voo-F9a7ZxJ',
